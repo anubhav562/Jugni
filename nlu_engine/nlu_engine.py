0045bb272f4da4b942f702caf75e89104e54ef36
Anubhav Chhabra <chhabra.anubhav1997@gmail.com>
2021-03-04 00:46:03 -0500

    [Jugni] - SnipsNLUEngine wrapper class

---
+++ nlu_engine/nlu_engine.py
@@ -0,0 +1,17 @@
+from snips_nlu import SnipsNLUEngine
+
+
+class NLUEngine:
+
+    def __init__(self):
+        self.nlu_engine_object = SnipsNLUEngine.from_path("persisted_engine")
+
+    def detect_intents_and_slots(self, sentence):
+        parsed_output = self.nlu_engine_object.parse(sentence)
+        intent_name = parsed_output.get("intent", {}).get("intentName")
+        slots = parsed_output.get("slots", [])
+
+        if len(slots):
+            slots = [{slot["slotName"]: slot["value"]["value"]} for slot in slots]
+
+        return intent_name, slots
